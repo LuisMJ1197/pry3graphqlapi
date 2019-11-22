53436905c87c004a862ef0e3cec41d8565966a9b
Luis Fernando Molina Juarez <Luismj138@hotmail.com>
2019-11-22 00:33:18 -0600

    Cambios realizados para publicación de API

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const graphql = require("graphql-yoga");
 const { GraphQLSchema } = graphql;
 const { resolvers } = require("./schema/resolvers");
-
+const localtunnel = require('localtunnel');
+ 
 const server = new graphql.GraphQLServer({
   typeDefs: `./schema/schema.graphql`,
   resolvers,
 });
 
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+tunnelFunc = (async () => {
+  const tunnel = await localtunnel({port: 4000, subdomain: "bindin"});
+  console.log("Localtunnel is on. URL: " + tunnel.url);
+});
+
+server.start(() => {
+  console.log(`Server is running on http://localhost:4000`);
+  tunnelFunc();
+  console.log("All init.");
+});
+
+ 
